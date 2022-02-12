90425c35c9d660b78a3b9509e1ff2e90d7256d47
tamararingas <tamriasdev@gmail.com>
2022-02-12 13:58:15 +0200

    complete Game component

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -66,12 +66,13 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
-      stepNumer: 0,
+      stepNumber: 0,
       xIsNext: true,
     };
   }
   handleClick(i){
-    const history = this.state.history;
+    const history = this.state.history.slice(0,
+      this.state.stepNumber + 1);
     const current = history[history.length -1];
     const squares = 
     current.squares.slice();
@@ -84,20 +85,21 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares,
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
   jumpTo(step){
     this.setState({
-      stepNumer: step,
+      stepNumber: step,
       xIsNext: (step%2) ===0,
     });
   }
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
 
     const moves = history.map((step, move) =>
