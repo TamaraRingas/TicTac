e9dc497bea054b8dac5b12ee4ec7a613070f33a5
tamararingas <tamriasdev@gmail.com>
2022-02-12 13:45:56 +0200

    map history to render() in Game

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -90,6 +90,21 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) =>
+    {
+      const desc = move ?
+        'Go to move ' + move :
+        'Go to game start';
+      return (
+        <li>
+          <button onClick={() =>
+          this.jumpTo(move)}>{desc}
+          </button>
+        </li>
+      );
+    });
+
     let status;
     if (winner){
       status = 'Winner: '+ winner;
@@ -102,12 +117,12 @@ class Game extends React.Component {
         <div className="game-board">
           <Board 
             squares={current.squares}
-            onClicl={(i) => this.handleClick(i)}
+            onClick={(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
