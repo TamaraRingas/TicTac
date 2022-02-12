d022db48a62a30571d3bd827da024cbe09b3bf4b
tamararingas <tamriasdev@gmail.com>
2022-02-12 12:59:48 +0200

    Initialize Game

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -79,10 +79,21 @@ class Game extends React.Component {
     };
   }
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner){
+      status = 'Winner: '+ winner;
+    } else {
+      status = 'Next player: ' + 
+      (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+          />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
